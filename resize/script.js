63776790d1f193633d9dd9a8881e228c3f193ccc
Jesse Strickland <112838688+Skinniez@users.noreply.github.com>
2023-03-25 17:14:25 -0400

    added resize unstyled

---
+++ resize/script.js
@@ -0,0 +1,72 @@
+const inputImage = document.getElementById('inputImage');
+const resizeButton = document.getElementById('resizeButton');
+const imageGrid = document.getElementById('imageGrid');
+
+const sizes = [128, 56, 28, 72, 36, 18];
+
+sizes.forEach((size) => {
+  const gridItem = document.createElement('div');
+  gridItem.className = 'image-grid-item';
+  gridItem.innerHTML = `<label>${size}x${size}</label>`;
+  imageGrid.appendChild(gridItem);
+});
+
+const imageGridItems = document.querySelectorAll('.image-grid-item');
+
+inputImage.addEventListener('change', () => {
+    if (inputImage.files && inputImage.files.length > 0) {
+      resizeButton.disabled = false;
+    } else {
+      resizeButton.disabled = true;
+    }
+  });
+  
+
+resizeButton.addEventListener('click', () => {
+  const file = inputImage.files[0];
+
+  if (!file) {
+    return;
+  }
+
+  sizes.forEach((size, index) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const img = new Image();
+      img.src = event.target.result;
+
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = size;
+        canvas.height = size;
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0, size, size);
+
+        const resizedDataURL = canvas.toDataURL(file.type);
+
+        const imageContainer = imageGridItems[index];
+        const existingImage = imageContainer.querySelector('img');
+        if (existingImage) {
+          existingImage.remove();
+        }
+
+        const previewImage = document.createElement('img');
+        previewImage.src = resizedDataURL;
+        previewImage.title = `Click to download ${size}x${size}`;
+        previewImage.addEventListener('click', () => {
+          const link = document.createElement('a');
+          link.href = resizedDataURL;
+          link.download = `resized_${size}.${file.type.split('/')[1]}`;
+          link.style.display = 'none';
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        });
+
+        imageContainer.appendChild(previewImage);
+      };
+    };
+
+    reader.readAsDataURL(file);
+  });
+});
